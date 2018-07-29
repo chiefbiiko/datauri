3b5a490c8089d359fb62ccc59a16ee78740c538b
Noah Anabiik Schwarz <noah.anabiik.schwarz@gmail.com>
2018-07-29 11:43:27 +0200

    simplified

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,23 @@
+// TODO: modularize with a library
+
 extern crate base64;
-extern crate regex;
 
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
 use std::ffi::OsStr;
-use regex::Regex;
-
-struct ExtensionRegex {
-    font: Regex,
-    img: Regex,
-    svg: Regex
-}
-
-// static ext_re: ExtensionRegex = ExtensionRegex {
-//     font: Regex::new(r"").unwrap(),
-//     img: Regex::new(r"(?:jpg|jpeg|png|svg|gif)$").unwrap(),
-//     svg: Regex::new(r"\\.svg$").unwrap()
-// };
 
 fn main () {
-    let ext_re: ExtensionRegex = ExtensionRegex { // TODO: make this static
-        font: Regex::new(r"").unwrap(), // TODO: allow GoogleFont font exetensions
-        img: Regex::new(r"(?:jpg|jpeg|png|svg|gif)$").unwrap(), // TODO: ignore case
-        svg: Regex::new(r"\\.svg$").unwrap() // TODO: ignore case
-    };
-
     let filename: String = parse_filename_arg();
-    let buf: Vec<u8> = read_file_buf(&filename);
-    println!("{}", concat_uri(&filename, &buf, &ext_re));
+    println!(
+        "data:image/{};base64,{}",
+        infer_img_type(&filename),
+        &base64::encode(&read_file_buf(&filename))
+    );
 }
 
-// TODO: move below to lib.rs
+// TODO: check argv types
 fn parse_filename_arg () -> String {
     let args: Vec<String> = env::args().collect::<Vec<String>>();
     if args.len() < 2 {
@@ -52,29 +37,10 @@ fn extract_extension(filename: &str) -> Option<&str> {
     Path::new(filename).extension().and_then(OsStr::to_str)
 }
 
-fn concat_uri (filename: &str, buf: &Vec<u8>, ext_re: &ExtensionRegex) -> String {
-    let media_t: &str;
-    let m_type: &str;
+fn infer_img_type (filename: &str) -> &str {
     let ext: &str = extract_extension(filename).expect("missing file extension");
-
-    // TODO: match
-    if ext_re.img.is_match(ext) {
-        media_t = "image";
+    match ext {
+        "svg" => "svg+xml",
+        _ => "*"
     }
-    // else if ext_re.font.is_match(ext) {
-    //     media_t = "font";
-    // }
-    else {
-        panic!("unknown file extension");
-    }
-
-    if ext_re.svg.is_match(ext) {
-        m_type = "svg+xml"
-    } else if media_t == "font" {
-        m_type = ext;
-    } else {
-        m_type = "*"
-    }
-
-    format!("data:{}/{};base64,{}", media_t, m_type, &base64::encode(buf))
 }
